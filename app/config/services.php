5595316531fb91449db3fbe274a98c3db580de83
Clement-Hellot <76108212+Clement-Hellot@users.noreply.github.com>
2021-02-01 15:30:27 +0100

    new / delete

--- app/config/services.php
+++ app/config/services.php
@@ -2,7 +2,7 @@
 use Ubiquity\controllers\Router;
 
 \Ubiquity\cache\CacheManager::startProd($config);
-\Ubiquity\orm\DAO::start();
+//\Ubiquity\orm\DAO::start();
 Router::start();
 Router::addRoute("_default", "controllers\\TodosController");
-\Ubiquity\assets\AssetsManager::start($config);
+//\Ubiquity\assets\AssetsManager::start($config);
--- app/controllers/TodosController.php
+++ app/controllers/TodosController.php
@@ -5,23 +5,27 @@ namespace controllers;
 use Ubiquity\attributes\items\router\Get;
 use Ubiquity\attributes\items\router\Post;
 use Ubiquity\attributes\items\router\Route;
+use Ubiquity\controllers\Router;
+use Ubiquity\utils\http\URequest;
+use Ubiquity\utils\http\USession;
 
 /**
  * Controller TodosController
  */
 
-class TodosController extends ControllerBase{
+class TodosController extends ControllerBase
+{
   const CACHE_KEY = 'datas/lists/';
-  const EMPTY_LIST_ID='not saved';
-  const LIST_SESSION_KEY='list';
-  const ACTIVE_LIST_SESSION_KEY='active-list';
+  const EMPTY_LIST_ID = 'not saved';
+  const LIST_SESSION_KEY = 'list';
+  const ACTIVE_LIST_SESSION_KEY = 'active-list';
 
-  #[Route(path:"_default", name:'home' )]
-  public function index(){
+  #[Route(path: "_default", name: 'home')]
+  public function index()
+  {
 
-    $this->displayList(['Test','Eau','Lait','Fruits','Bonbons']);
-    $this->showMessage('Bienvenue !','TodoLists permet de gerer des listes...','info','info circle outline');
-
+    $this->showMessage('Bienvenue !', 'TodoLists permet de gerer des listes...', 'info', 'info circle outline');
+    $this->displayList(USession::get(self::LIST_SESSION_KEY));
   }
 
 
@@ -32,65 +36,95 @@ class TodosController extends ControllerBase{
   }
 
 
-  #[Post(path: "todo/loadList/",name: "todo.loadListPost")]
-	public function loadListFromForm(){
-
-	}
-
-#[Get(path: "todo/loadList/{uniqid}",name: "todo.loadList")]
-	public function loadList(int $uniqid){
-
-}
-
-  #[Get(path: "todo/saveList",name: "todo.save")]
-  public function saveList(){
-
+  #[Post(path: "todos/loadList/", name: "todos.loadListPost")]
+  public function loadListFromForm()
+  {
   }
-	#[Post(path: "todo/new/{force}",name: "todo.new")]
-  public function newlist($force = false){
-    if($force != false | !USession::exists(self::LIST_SESSION_KEY)){
+
+  #[Get(path: "todos/loadList/{uniqid}", name: "todos.loadList")]
+  public function loadList(int $uniqid)
+  {
+  }
+
+  #[Get(path: "todos/saveList", name: "todos.save")]
+  public function saveList()
+  {
+  }
+  #[Get(path: "todos/new/{force}", name: "todos.new")]
+  public function newlist($force = false)
+  {
+    if ($force != false || !USession::exists(self::LIST_SESSION_KEY)) {
       USession::set(self::LIST_SESSION_KEY, []);
-      $this->displayList(USession::get(self::LIST_SESSION_KEY));
-    }else if(USession::exists(self::LIST_SESSION_KEY)) {
-      $this->showMessage("Nouvelle Liste", "Une liste existe déjà. Voulez vous la vider ?", "", "",
-        [['url' =>Router::path('todos.new/1'),'caption'=>'Créer une nouvelle liste','style'=>'basic inverted'],
-          ['url' =>Router::path('todos.menu'),'caption'=>'Annuler','style'=>'basic inverted']]);
-      $this->displayList(USession::get(self::LIST_SESSION_KEY));
+      $this->showMessage("Liste correctement crée", self::LIST_SESSION_KEY . " a été correctement crée", "", "");
+    } else if (USession::exists(self::LIST_SESSION_KEY)) {
+      $this->showMessage(
+        "Nouvelle Liste",
+        "Une liste existe déjà. Voulez vous la vider ?",
+        "",
+        "",
+        [
+          ['url' => Router::path('todos.new',[1]), 'caption' => 'Créer une nouvelle liste', 'style' => 'basic inverted'],
+          ['url' => Router::path('todos.menu'), 'caption' => 'Annuler', 'style' => 'basic inverted']
+        ]
+      );
+      
     }
+    $this->displayList(USession::get(self::LIST_SESSION_KEY));
   }
 
 
-	#[Post(path: "todo/addElement",name: "todo.add")]
-	public function addElement(){
 
-	}
+  #[Post(path: "todos/add", name: "todos.add")]
+  public function addElement()
+  {
 
-
-	#[Get(path: "todo/deleteElement/{index}",name: "todo.delete")]
-	public function deleteElement($index){
-
-	}
-
-
-	#[Post(path: "todo/editElement/{index}",name: "todo.edit")]
-	public function editElement($index){
-
-	}
-
-
-
-	public function menu(){
-
-		$this->loadView('TodosController/menu.html');
-
-	}
-
-  private function displayList($list){
-    $this->loadView('TodosController/displayList.html', ['list'=>$list]);
+    $list = USession::get(self::LIST_SESSION_KEY);
+    if (URequest::filled('elements')) {
+      $elemnts = explode("\n", URequest::post('elements'));
+      foreach ($elemnts as $elm) {
+        $list[] = $elm;
+      }
+    } else {
+      $list[] = URequest::post('element');
+    }
+    USession::set(self::LIST_SESSION_KEY, $list);
+    $this->displayList($list);
   }
 
-  private function showMessage(string $header, string $message, string $type = '', string $icon = 'info circle',array $buttons=[]) {
-    $this->loadView('TodosController/showMessage.html', compact('header', 'type', 'icon', 'message','buttons'));
+
+  #[Get(path: "todos/deleteElement/{index}", name: "todos.delete")]
+  public function deleteElement($index)
+  {
+    $list = USession::get(self::LIST_SESSION_KEY);
+    array_splice($list, $index, 1);
+    USession::set(self::LIST_SESSION_KEY, $list);
+    $this->displayList($list);
+  }
+
+
+  #[Post(path: "todos/editElement/{index}", name: "todos.edit")]
+  public function editElement($index)
+  {
+    $list = USession::get(self::LIST_SESSION_KEY);
+    $list[$index] = URequest::post('element');
+    USession::set(self::LIST_SESSION_KEY, $list);
+  }
+
+
+  public function menu()
+  {
+
+    $this->loadView('TodosController/menu.html');
+  }
+
+  private function displayList($list)
+  {
+    $this->loadView('TodosController/displayList.html', ['list' => $list]);
+  }
+
+  private function showMessage(string $header, string $message, string $type = '', string $icon = 'info circle', array $buttons = [])
+  {
+    $this->loadView('TodosController/showMessage.html', compact('header', 'type', 'icon', 'message', 'buttons'));
   }
 }
 //
