65e1ce639deaeac8464e0f327ba05b1194ccc3ea
Clement Hellot <clement.hellot.etu@gmail.com>
2021-02-22 21:27:42 +0100

    Maj composer

---
+++ app/vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '38fb5d743c02c2c518f7a0ce3d6f0680cc5b2c00',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '38fb5d743c02c2c518f7a0ce3d6f0680cc5b2c00',
+    ),
+    'phpmv/ubiquity' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '2.4.x-dev',
+      ),
+      'reference' => '1c587f43416cc8c336be285e2a943affadc9aa64',
+    ),
+  ),
+);
