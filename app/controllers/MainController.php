fa9399cae21b50feaf443af24b71720c1296d06a
Clement Hellot <clement.hellot.etu@gmail.com>
2021-03-22 17:42:46 +0100

    store

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -3,10 +3,12 @@
 namespace controllers;
 
 use models\Product;
+use models\Section;
 use Ubiquity\attributes\items\router\Route;
 use Ubiquity\controllers\auth\AuthController;
 use Ubiquity\controllers\auth\WithAuthTrait;
 use Ubiquity\orm\DAO;
+use Ubiquity\utils\http\USession;
 
 /**
  * Controller MainController
@@ -25,5 +27,23 @@ class MainController extends ControllerBase
 	protected function getAuthController(): AuthController
 	{
 		return new MyAuth($this);
+	}
+
+	#[Route('store', name: 'store')]
+	public function store()
+	{
+		$store = DAO::getAll(Product::class, false, false);
+		$sections = DAO::getAll(Section::class, '', ['products']);
+		$promos = DAO::getAll(Product::class, 'promotion< ?', false, [0]);
+		$this->loadView('MainController/store.html', ['store' => $store, 'promo' => $promos, 'sections' => $sections]);
+	}
+
+	#[Route('section/{id}', name: 'section')]
+	public function section($id)
+	{
+		$product = DAO::getAll(Product::class, 'idSection= ' . $id, [USession::get("idSection")]);
+		$section = DAO::getById(Section::class, $id, ['products']);
+		$sections = DAO::getAll(Section::class, '', ['products']);
+		$this->loadDefaultView(['section' => $section, 'sections' => $sections, 'product' => $product]);
 	}
 }
